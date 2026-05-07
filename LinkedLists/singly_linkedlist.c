56f6d33dafa1a964ea86f48e55f73915d82a1899
Nimisha-Budati <nimishabudati2006@gmail.com>
2026-05-07 12:40:04 +0000

    creation and traversal of a singly linked list

---
+++ LinkedLists/singly_linkedlist.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *head=NULL;
+void traversal(){
+    if(head==NULL){
+        printf("Linked List is empty\n");
+        return;
+    }
+    struct node *temp=head;
+    while(temp!=NULL){
+        printf("%d -> ",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+}
+void createNode(){
+    struct node *newnode,*temp;
+    int value;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL){
+        printf("Memory allocation failed\n");
+        return;
+    }
+    printf("Enter data: ");
+    scanf("%d",&value);
+    newnode->data=value;
+    newnode->next=NULL;
+    if(head==NULL){
+        head=newnode;
+    }
+    else{
+        temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+    printf("Node created successfully\n");
+}
+int main(){
+    int choice;
+    do{
+        printf("\n===== MENU =====\n");
+        printf("0.Exit\n");
+        printf("1.Traversal\n");
+        printf("2.Create New Node\n");
+        printf("Enter choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 0:
+                printf("Exiting program...\n");
+                break;
+            case 1:
+                traversal();
+                break;
+            case 2:
+                createNode();
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    }while(choice!=0);
+    struct node *temp=head;
+    while(temp!=NULL){
+        struct node *nextnode=temp->next;
+        free(temp);
+        temp=nextnode;
+    }
+    return 0;
+}
