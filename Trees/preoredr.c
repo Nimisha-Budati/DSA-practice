3a99d9b5d00baf4a689360c1349a7724faa00dc1
Nimisha-Budati <nimishabudati2006@gmail.com>
2026-05-28 17:22:34 +0000

    Added preorder traversal to binary tree

---
+++ Trees/preoredr.c
@@ -0,0 +1,83 @@
+//Added preorder traversal
+//Implementation of binary tree with memory deallocation
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node* create(){
+    int x;
+    printf("Enter data (-1 for no node): ");
+    scanf("%d",&x);
+    if(x==-1){
+        return NULL;
+    }
+    struct node *newnode;
+    newnode=(struct node*)malloc(sizeof(struct node));
+    if(newnode==NULL){
+        printf("Memory allocation failed\n");
+        return NULL;
+    }
+    newnode->data=x;
+    printf("Enter left child of %d\n",x);
+    newnode->left=create();
+    printf("Enter right child of %d\n",x);
+    newnode->right=create();
+    return newnode;
+}
+void preorderTraversal(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    printf("%d ",root->data);
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+void freetree(struct node *root){
+    if(root==NULL){
+        return;
+    }
+    freetree(root->left);
+    freetree(root->right);
+    free(root);
+}
+int main(){
+    struct node *root=NULL;
+    int choice;
+    do{
+        printf("\n====== MENU ======\n");
+        printf("0. Exit\n");
+        printf("1. Create Tree\n");
+        printf("2. Preorder Traversal\n");
+        printf("Enter choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+             case 0:
+                freetree(root);
+                root=NULL;
+                printf("Program exited successfully\n");
+                break;
+            case 1:
+                freetree(root);
+                root=NULL;
+                printf("\nCreate Binary Tree\n");
+                root=create();
+                break;
+            case 2:
+                if(root==NULL){
+                    printf("Tree is empty\n");
+                }
+                else{
+                    printf("Preorder Traversal: ");
+                    preorderTraversal(root);
+                    printf("\n");
+                }
+                break;
+            default:
+                printf("Invalid choice\n");
+        }
+    }while(choice!=0);
+    return 0;
+}
