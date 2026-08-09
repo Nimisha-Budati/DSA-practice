834d9185415f9c9d32a73eee9c17a3f815471b9c
Nimisha-Budati <nimishabudati2006@gmail.com>
2026-08-09 05:59:19 +0000

    Removing the lowest significant bit

--- BitManuplation/remove_the_lowest_set_bit.c
+++ BitManuplation/remove_the_lowest_set_bit.c
@@ -3,7 +3,13 @@
 int main(){
     int a=12;
     printf("12 in binary form 1100.\n");
-    printf("So 12 after removing lowest significant bit is 11 i.e, 1000.\n");
+    printf("So 12 after removing lowest significant bit is 8 i.e, 1000.\n");
     printf("12 -> %d\n",a&(a-1));
     return 0;
 }
+/*
+  1100
+& 1011
+---------
+  1000
+*/
