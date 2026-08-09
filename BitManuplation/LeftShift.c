31b8dd060e8eb835df4d03c606e3622f4328f58c
Nimisha-Budati <nimishabudati2006@gmail.com>
2026-08-09 05:19:33 +0000

    Bit manuplation - Left Shift

---
+++ BitManuplation/LeftShift.c
@@ -0,0 +1,16 @@
+/*
+Left Shift << ⭐
+Left shift moves bits toward the left.
+*/
+#include<stdio.h>
+int main(){
+    int a=5;
+    printf("5 << 1: %d\n",a<<1);
+    return 0;
+}
+/*
+For positive integers:
+n << 1 = n × 2
+n << 2 = n × 4
+n << 3 = n × 8
+*/
