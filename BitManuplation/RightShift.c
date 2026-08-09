993c050497d665833ee4c0afbd0f9df86e10576f
Nimisha-Budati <nimishabudati2006@gmail.com>
2026-08-09 05:21:55 +0000

    Bit manuplation - Right Shift

---
+++ BitManuplation/RightShift.c
@@ -0,0 +1,11 @@
+/*Right Shift >>
+Right shift moves bits toward the right.*/
+#include<stdio.h>
+int main(){
+    int a=5;
+    printf("5 >> 1: %d\n",a>>1);
+    return 0;
+}
+/*
+n >> 1 ≈ n / 2
+*/
